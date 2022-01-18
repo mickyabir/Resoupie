cd5836bfb5f8c3f077c4d01a308d7e0e208128b7
Micky Abir <micky.abir@gmail.com>
2022-01-18 13:05:44 -0800

    Initial Commit

---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Michael Abir on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
